76efba3d81d11563364ff3c8b72ea7cdad5fd88f
koksoybedirhan <71734713+koksoybedirhan@users.noreply.github.com>
2022-07-01 23:38:24 +0300

    Add files via upload

---
+++ EVA-X-Backup-Avionic/EVA-X-Backup-Ground-Station.ino
@@ -0,0 +1,24 @@
+String okunan;
+char son[50];
+
+void setup() {
+  Serial.begin(9600);
+  Serial1.begin(9600);
+}
+
+void loop() 
+{
+  if (Serial.available() > 0) 
+  {    
+    okunan = Serial.readString();
+    okunan.toCharArray(son, 50);
+    if(son[0] == "E" && son[1] == "V" && son[2] == "A") 
+    {//Gelen Verilerin EVA X takımı tarafından geldiğinin kontrolü
+      Serial.println(son);
+    }
+    else
+    {
+      Serial.println("Istenilen Veri Tipi Gelmedi");
+    }
+  }
+}

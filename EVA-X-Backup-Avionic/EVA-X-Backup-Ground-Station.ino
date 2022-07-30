f03b9f4d27d393a539eb11c612b1fe7f99046293
koksoybedirhan <71734713+koksoybedirhan@users.noreply.github.com>
2022-07-30 14:56:11 +0300

    Update EVA-X-Backup-Ground-Station.ino

--- EVA-X-Backup-Avionic/EVA-X-Backup-Ground-Station.ino
+++ EVA-X-Backup-Avionic/EVA-X-Backup-Ground-Station.ino
@@ -1,24 +1,18 @@
-String okunan;
-char son[50];
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+SoftwareSerial dorji(11,10);
 
 void setup() 
 {
   Serial.begin(9600);
+  dorji.begin(9600);
 }
 
 void loop() 
 {
-  if (Serial.available() > 0) 
+  if(dorji.available()>0) 
   {    
-    okunan = Serial.readString();
-    okunan.toCharArray(son, 50);
-    if(son[0] == 'E' && son[1] == 'V' && son[2] == 'A') 
-    {//Gelen Verilerin EVA X takımı tarafından geldiğinin kontrolü
-      Serial.println(son);
-    }
-    else
-    {
-      Serial.println("Istenilen Veri Tipi Gelmedi");
-    }
+    Serial.write(dorji.read());
   }
+  delay(10);
 }
